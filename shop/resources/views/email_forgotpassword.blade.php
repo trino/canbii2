4144e86a651ba1e7d0e74c89331ee87ef85a310a
trino <info@trinoweb.com>
2018-11-04 19:48:36 -0500

    delete shop

--- shop/resources/views/email_forgotpassword.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts_email')
-@section('header')
-    Forgot Password
-@endsection
-@section('content')
-<!-- Remember, no CSS can be used -->
-Your new password is <?= $password; ?>
-@endsection
